1bbff6f54cc62080266e40b44ea4eb400053f680
FelipeSpinola008 <felipespinola784@gmail.com>
2025-03-12 11:57:48 -0300

    adicionando funções ao site

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let seuAmigo = [];
+
+function adicionarAmigo() {
+    let inputNome = document.getElementById('amigo');
+    let adicionar = inputNome.value.trim();
+    
+    if (adicionar === "") {
+        alert('digite um nome valido');
+        return;
+    }
+    seuAmigo.push(adicionar);
+    inputNome.value = "";
+    inputNome.focus();
+    listaAtualizada();
+}
+
+//adiciona os nomes em uma lista para ser sorteado
+function listaAtualizada() {
+    let atualizarNomes = document.getElementById('listaAmigos');
+    atualizarNomes.innerHTML = ""; 
+
+    
+    for ( let atualizarLista = 0; atualizarLista < seuAmigo.length; atualizarLista++ ) {
+        let item = document.createElement('li');
+        item.textContent = seuAmigo[atualizarLista];
+        atualizarNomes.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if(seuAmigo === "") {
+        alert('nehum nome adicionado');
+        return;
+    }
+    let sorteio = seuAmigo[Math.floor(Math.random() * seuAmigo.length)];
+    let vencedor = document.getElementById('resultado');
+    vencedor.innerHTML = `O nome sorteado foi ${sorteio}`;
+
+    let limparLista = document.getElementById('listaAmigos');
+    limparLista.innerHTML = "";
+    seuAmigo = [];
+}
+
